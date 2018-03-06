4f263f104e7973290093605a063f353b03bf4210
Martin Vysny <mavi@vaadin.com>
2018-03-06 11:27:36 +0200

    Added tests for Templates

---
+++ karibu-testing-v10/src/test/kotlin/com/github/karibu/testing/ReviewsList.kt
@@ -0,0 +1,30 @@
+package com.github.karibu.testing
+
+import com.vaadin.flow.component.Tag
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.dependency.HtmlImport
+import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.polymertemplate.Id
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.router.PageTitle
+import com.vaadin.flow.templatemodel.TemplateModel
+
+/**
+ * Tests that the Browserless testing approach is able to instantiate a polymer template properly.
+ */
+@PageTitle("Review List")
+@Tag("reviews-list")
+@HtmlImport("frontend://reviews-list.html")
+class ReviewsList : PolymerTemplate<TemplateModel>() {
+
+    @Id("search")
+    internal lateinit var search: TextField
+    @Id("newReview")
+    internal lateinit var addReview: Button
+    @Id("header")
+    internal lateinit var header: H1
+
+    interface ReviewsModel : TemplateModel {
+    }
+}

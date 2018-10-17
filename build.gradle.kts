d19a3622e7466732afcb9f4630e3d355b55678b8
Martin Vysny <martin@vysny.me>
2018-10-17 11:00:15 +0300

    0.5.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.kaributesting"
-    version = "0.5.0"
+    version = "0.5.1-SNAPSHOT"
     repositories {
         jcenter()
         maven { setUrl("https://dl.bintray.com/mvysny/github") }

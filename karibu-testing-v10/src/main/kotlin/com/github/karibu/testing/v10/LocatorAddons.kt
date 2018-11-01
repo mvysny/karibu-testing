646f82999530b030d98c7fc619a002ebf4602975
Martin Vysny <martin@vysny.me>
2018-11-01 08:47:52 +0200

    Vaadin 10: add HasText.textContains predicate

--- karibu-testing-v10/src/main/kotlin/com/github/karibu/testing/v10/LocatorAddons.kt
+++ karibu-testing-v10/src/main/kotlin/com/github/karibu/testing/v10/LocatorAddons.kt
@@ -1,6 +1,7 @@
 package com.github.karibu.testing.v10
 
 import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.HasText
 import java.util.function.Predicate
 
 /**
@@ -13,4 +14,16 @@ fun <T : Component> SearchSpec<T>.captionContains(substring: String) {
 private data class CaptionContainsPredicate<T : Component>(val substring: String) : Predicate<T> {
     override fun test(t: T): Boolean = t.caption.contains(substring)
     override fun toString() = "captionContains('$substring')"
+}
+
+/**
+ * Makes sure that the component's [HasText.getText] contains given [substring].
+ */
+fun <T> SearchSpec<T>.textContains(substring: String) where T : HasText, T : Component {
+    predicates.add(TextContainsPredicate(substring))
+}
+
+private data class TextContainsPredicate<T : HasText>(val substring: String) : Predicate<T> {
+    override fun test(t: T): Boolean = t.text.contains(substring)
+    override fun toString() = "textContains('$substring')"
 }

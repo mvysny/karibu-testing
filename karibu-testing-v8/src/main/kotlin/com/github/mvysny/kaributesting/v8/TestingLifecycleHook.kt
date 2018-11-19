d1cb41618d42561b1a00e40ec95a3e9b308b6a47
Martin Vysny <martin@vysny.me>
2018-11-19 09:28:14 +0200

    docs

--- karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/TestingLifecycleHook.kt
+++ karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/TestingLifecycleHook.kt
@@ -1,5 +1,9 @@
 package com.github.mvysny.kaributesting.v8
 
+/**
+ * If you need to hook into the testing lifecycle (e.g. you need to wait for any async operations to finish),
+ * provide your own custom implementation of this interface, then set it into [testingLifecycleHook].
+ */
 interface TestingLifecycleHook {
     /**
      * Invoked before every component lookup. You can e.g. wait for any async operations to finish and for the server to settle down.
@@ -20,6 +24,6 @@ interface TestingLifecycleHook {
 /**
  * If you need to hook into the testing lifecycle (e.g. you need to wait for any async operations to finish),
  * set your custom implementation here. See [TestingLifecycleHook] for more info on
- * where exactly you can hook into.
+ * where exactly you can hook into. Defaults to [TestingLifecycleHook.noop].
  */
 var testingLifecycleHook: TestingLifecycleHook = TestingLifecycleHook.noop

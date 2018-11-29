93195b6c38771fa7c373e970d70d6caa3b192f79
Martin Vysny <martin@vysny.me>
2018-11-29 09:23:25 +0200

    downgraded bintray plugin to workaround for NPE

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import java.util.*
 
 plugins {
     kotlin("jvm") version "1.3.10"
-    id("com.jfrog.bintray") version "1.8.4"
+    id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
     id("org.jetbrains.dokka") version "0.9.17"
 }

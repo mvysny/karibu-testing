e8b190155154ccd95906771b8cc24442dec66f31
Martin Vysny <martin@vysny.me>
2018-12-04 12:13:00 +0200

    v bump

--- karibu-testing-v10/kt10-testrun-vaadin12/build.gradle.kts
+++ karibu-testing-v10/kt10-testrun-vaadin12/build.gradle.kts
@@ -1,7 +1,3 @@
-repositories {
-    maven { setUrl("https://maven.vaadin.com/vaadin-prereleases") }
-}
-
 dependencies {
     compile(platform("com.vaadin:vaadin-bom:${ext["vaadin12_version"]}"))
     compile("com.vaadin:vaadin-core:${ext["vaadin12_version"]}")

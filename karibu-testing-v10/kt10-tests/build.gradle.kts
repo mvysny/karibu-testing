144e2891cc1eee9132ee1cd7361f85edfce5a5f8
Martin Vysny <martin@vysny.me>
2018-12-04 09:30:40 +0200

    optimize dependency tree

--- karibu-testing-v10/kt10-tests/build.gradle.kts
+++ karibu-testing-v10/kt10-tests/build.gradle.kts
@@ -1,8 +1,7 @@
 dependencies {
-    compile(platform("com.vaadin:vaadin-bom:${ext["vaadin_platform_lts_version"]}"))
-    compile("com.vaadin:vaadin-core:${ext["vaadin_platform_lts_version"]}")
     compile(project(":karibu-testing-v10"))
 
+    // for testing purposes
     compile("com.github.mvysny.dynatest:dynatest-engine:${ext["dynatest_version"]}")
     compile("org.slf4j:slf4j-simple:1.7.25")
     compile("com.github.mvysny.karibudsl:karibu-dsl-v10:0.5.0")

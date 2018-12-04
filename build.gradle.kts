e4f9f3e08652e5520fd8cefe07acd6958d7f97b6
Martin Vysny <martin@vysny.me>
2018-12-04 12:55:19 +0200

    1.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.0.1"
+    version = "1.0.2-SNAPSHOT"
     repositories {
         jcenter()
     }

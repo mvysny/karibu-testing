94a0ba83a23bbd9e4160b148b82fdaae39218488
Martin Vysny <martin@vysny.me>
2019-02-12 07:25:59 +0200

    Add PrettyPrintTree test for ContextMenu

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/PrettyPrintTreeTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/PrettyPrintTreeTest.kt
@@ -2,23 +2,31 @@ package com.github.mvysny.kaributesting.v10
 
 import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.karibudsl.v10.div
 import com.github.mvysny.karibudsl.v10.text
 import com.vaadin.flow.component.Text
+import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.contextmenu.ContextMenu
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.textfield.TextArea
 import com.vaadin.flow.component.textfield.TextField
 import kotlin.test.expect
+import kotlin.test.fail
 
 internal fun DynaNodeGroup.prettyPrintTreeTest() {
+    beforeEach { MockVaadin.setup(Routes().autoDiscoverViews("com.github")) }
+    afterEach { MockVaadin.tearDown() }
+
     test("Simple dump") {
         val div = Div().apply {
             text("Foo")
         }
-        expect("""└── Div[text='Foo']
+        expect("""
+└── Div[text='Foo']
     └── Text[text='Foo']
-""") { div.toPrettyTree() }
+""".trim()) { div.toPrettyTree().trim() }
     }
 
     test("toPrettyString()") {
@@ -29,5 +37,23 @@ internal fun DynaNodeGroup.prettyPrintTreeTest() {
         expect("TextArea[label='label', value='some text']") { TextArea("label").apply { value = "some text" } .toPrettyString() }
         expect("Grid[]") { Grid<Any>().toPrettyString() }
         expect("Column[header='My Header']") { Grid<Any>().run { addColumn { it } .apply { header2 = "My Header" } }.toPrettyString() }
+    }
+
+    test("menu dump") {
+        lateinit var cm: ContextMenu
+        UI.getCurrent().div {
+            cm = contextMenu {
+                item("menu") {
+                    isEnabled = false
+                    item("click me", { e -> fail("shouldn't be called") })
+                }
+                item("save as")
+            }
+        }
+        expect("""
+└── ContextMenu[]
+    ├── MenuItem[DISABLED, text='menu']
+    │   └── MenuItem[text='click me']
+    └── MenuItem[text='save as']""".trim()) { cm.toPrettyTree().trim() }
     }
 }

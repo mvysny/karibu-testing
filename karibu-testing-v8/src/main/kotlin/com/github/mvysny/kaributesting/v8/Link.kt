090764e6d38d02ad88f2fb07c34daf1a303ef967
Martin Vysny <martin@vysny.me>
2019-03-04 15:42:45 +0200

    Initial support for Resource.download() and Link.download()

--- karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/Link.kt
+++ karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/Link.kt
@@ -1,7 +1,9 @@
 package com.github.mvysny.kaributesting.v8
 
+import com.vaadin.server.ConnectorResource
 import com.vaadin.server.ExternalResource
 import com.vaadin.server.Resource
+import com.vaadin.server.ThemeResource
 import com.vaadin.ui.Link
 import com.vaadin.ui.UI
 
@@ -22,8 +24,38 @@ fun Link._click() {
  * This only works for links with the [Link.getResource] containing [ExternalResource] which points to a View.
  */
 fun Link.click() {
-    val res: Resource = resource ?: throw AssertionError("${this.toPrettyString()}: resource is null")
-    if (res !is ExternalResource) throw AssertionError("${this.toPrettyString()}: resource $res is not ExternalResource")
+    val res: Resource = resource ?: throw AssertionError("${toPrettyString()}: resource is null")
+    if (res !is ExternalResource) throw AssertionError("${toPrettyString()}: resource $res is not ExternalResource")
     val url = res.url
     UI.getCurrent().navigator.navigateTo(url)
 }
+
+fun Link._download(): ByteArray {
+    checkEditableByUser()
+    return download()
+}
+
+fun Link.download(): ByteArray {
+    val res: Resource = resource ?: throw AssertionError("${toPrettyString()}: resource is null")
+    return when (res) {
+        is ThemeResource -> res.download()
+        is ConnectorResource -> res.download()
+        else -> throw AssertionError("")
+    }
+}
+
+fun Resource.download(): ByteArray = when (this) {
+    is ThemeResource -> download()
+    is ConnectorResource -> download()
+    else -> throw AssertionError("Cannot download from resource $this: unsupported type of resource")
+}
+
+fun ThemeResource.download(): ByteArray {
+    val theme = UI.getCurrent().theme!!
+
+}
+
+/**
+ * Downloads the contents of this [ConnectorResource]
+ */
+fun ConnectorResource.download(): ByteArray = stream.stream.use { it.readBytes() }

2f65e9430373a4cd717264a2db3c3ee66f179d61
Martin Vysny <martin@vysny.me>
2019-05-09 13:36:16 +0300

    1.1.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.1.6-SNAPSHOT"
+    version = "1.1.6"
     repositories {
         jcenter()
     }

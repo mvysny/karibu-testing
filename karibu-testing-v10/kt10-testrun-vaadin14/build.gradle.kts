793192074e0556d48db32d9e19a5607a11651b8c
Martin Vysny <martin@vysny.me>
2019-05-15 09:02:13 +0200

    Initial support for Vaadin 14

--- karibu-testing-v10/kt10-testrun-vaadin14/build.gradle.kts
+++ karibu-testing-v10/kt10-testrun-vaadin14/build.gradle.kts
@@ -1,5 +1,10 @@
+repositories {
+    jcenter()
+    maven { setUrl("https://maven.vaadin.com/vaadin-prereleases/") }
+}
+
 dependencies {
-    compile(platform("com.vaadin:vaadin-bom:${properties["vaadin12_version"]}"))
-    compile("com.vaadin:vaadin-core:${properties["vaadin12_version"]}")
+    compile(platform("com.vaadin:vaadin-bom:${properties["vaadin14_version"]}"))
+    compile("com.vaadin:vaadin-core:${properties["vaadin14_version"]}")
     compile(project(":karibu-testing-v10:kt10-tests"))
 }

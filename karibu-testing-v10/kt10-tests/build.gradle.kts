f72149452850922624901f7188d88eb45ca3521b
Martin Vysny <martin@vysny.me>
2019-07-22 15:22:12 +0300

    v bump

--- karibu-testing-v10/kt10-tests/build.gradle.kts
+++ karibu-testing-v10/kt10-tests/build.gradle.kts
@@ -5,4 +5,5 @@ dependencies {
     compile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
     compile("org.slf4j:slf4j-simple:1.7.25")
     compile("com.github.mvysny.karibudsl:karibu-dsl-v10:${properties["karibudsl_version"]}")
+    compileOnly("com.vaadin:vaadin-core:${properties["vaadin14_version"]}")
 }

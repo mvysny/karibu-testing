30782e1862175c2a7fc4087ca1a59c382656b447
Martin Vysny <martin@vysny.me>
2019-08-29 17:41:30 +0300

    vaadin 10 tests: assert on environment

--- karibu-testing-v10/kt10-testrun-vaadin14/src/test/kotlin/AllTests.kt
+++ karibu-testing-v10/kt10-testrun-vaadin14/src/test/kotlin/AllTests.kt
@@ -1,11 +1,19 @@
 import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.kaributesting.v10.MockVaadin
 import com.github.mvysny.kaributesting.v10.allTests
 import com.github.mvysny.kaributesting.v10.vaadinVersion
+import com.vaadin.flow.server.VaadinService
 import kotlin.test.expect
 
 class AllTests : DynaTest({
-    test("Vaadin version") {
-        expect(14) { vaadinVersion }
+    group("Vaadin env") {
+        beforeEach { MockVaadin.setup() }
+        afterEach { MockVaadin.tearDown() }
+
+        test("Vaadin version") {
+            expect(14) { vaadinVersion }
+            expect(true) { VaadinService.getCurrent().deploymentConfiguration.isCompatibilityMode }
+        }
     }
     allTests()
 })

089f1e910d75b8f7fb81c89ce30e6b914621fb7a
Martin Vysny <martin@vysny.me>
2019-10-17 11:41:50 +0300

    fixed tests on java 13

--- karibu-testing-v10/kt10-testrun-vaadin14-npm/src/test/kotlin/AllTests.kt
+++ karibu-testing-v10/kt10-testrun-vaadin14-npm/src/test/kotlin/AllTests.kt
@@ -7,23 +7,22 @@ import com.vaadin.flow.server.VaadinService
 import kotlin.test.expect
 
 class AllTests : DynaTest({
-    group("Vaadin env") {
-        beforeEach { MockVaadin.setup() }
-        afterEach { MockVaadin.tearDown() }
-
-        test("flow-build-info.json exists") {
-            val res = Thread.currentThread().contextClassLoader.getResource("META-INF/VAADIN/config/flow-build-info.json")
-            expect(true, "flow-build-info.json is not on the classpath!") { res != null }
-        }
-
-        test("Vaadin version") {
-            expect(14) { VaadinMeta.version }
-            expect(false) { VaadinMeta.isCompatibilityMode }
-            expect(false) { VaadinService.getCurrent().deploymentConfiguration.isCompatibilityMode }
-        }
-    }
-
     if (jvmVersion < 13) {
+        group("Vaadin env") {
+            beforeEach { MockVaadin.setup() }
+            afterEach { MockVaadin.tearDown() }
+
+            test("flow-build-info.json exists") {
+                val res = Thread.currentThread().contextClassLoader.getResource("META-INF/VAADIN/config/flow-build-info.json")
+                expect(true, "flow-build-info.json is not on the classpath!") { res != null }
+            }
+
+            test("Vaadin version") {
+                expect(14) { VaadinMeta.version }
+                expect(false) { VaadinMeta.isCompatibilityMode }
+                expect(false) { VaadinService.getCurrent().deploymentConfiguration.isCompatibilityMode }
+            }
+        }
         allTests()
     } else {
         // Sorry, no support for Vaadin 14+npm on Java 13 or higher: https://github.com/mvysny/karibu-testing/issues/29
--- karibu-testing-v10/kt10-testrun-vaadin14-npm/src/test/kotlin/NpmPolymerTemplateTest.kt
+++ karibu-testing-v10/kt10-testrun-vaadin14-npm/src/test/kotlin/NpmPolymerTemplateTest.kt
@@ -1,12 +1,17 @@
 import com.github.appreciated.app.layout.component.applayout.LeftLayouts
 import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.kaributesting.v10.MockVaadin
+import com.github.mvysny.kaributesting.v10.jvmVersion
 
 class NpmPolymerTemplateTest : DynaTest({
-    beforeEach { MockVaadin.setup() }
-    afterEach { MockVaadin.tearDown() }
+    if (jvmVersion < 13) {
+        beforeEach { MockVaadin.setup() }
+        afterEach { MockVaadin.tearDown() }
 
-    test("Instantiate LeftHybrid") {
-        LeftLayouts.LeftHybrid()
+        test("Instantiate LeftHybrid") {
+            LeftLayouts.LeftHybrid()
+        }
+    } else {
+        // Sorry, no support for Vaadin 14+npm on Java 13 or higher: https://github.com/mvysny/karibu-testing/issues/29
     }
 })

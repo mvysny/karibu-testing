7c40527a3ab6407e301625308fc5cd7afc17ead6
Martin Vysny <mavi@vaadin.com>
2019-11-27 13:35:15 +0200

    minor

--- karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/Button.kt
+++ karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/Button.kt
@@ -1,13 +1,22 @@
+@file:Suppress("FunctionName")
+
 package com.github.mvysny.kaributesting.v10
 
 import com.vaadin.flow.component.button.Button
 
 /**
  * Clicks the button, but only if it is actually possible to do so by the user. If the button is read-only or disabled, it throws an exception.
- * @throws IllegalArgumentException if the button was not visible, not enabled, read-only. See [checkEditableByUser] for
+ * @throws IllegalArgumentException if the button was not visible, not enabled, read-only. See [_checkClickable] for
  * more details.
  */
 fun Button._click() {
-    checkEditableByUser()
+    _checkClickable()
     click()  // this doesn't work on Vaadin 12 but it works properly with Vaadin 13
+}
+
+/**
+ * Alias for [checkEditableByUser].
+ */
+fun Button._checkClickable() {
+    checkEditableByUser()
 }
--- karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/Button.kt
+++ karibu-testing-v8/src/main/kotlin/com/github/mvysny/kaributesting/v8/Button.kt
@@ -5,10 +5,20 @@ package com.github.mvysny.kaributesting.v8
 import com.vaadin.ui.Button
 
 /**
- * Clicks the button, but only if it is actually possible to do so by the user. If the button is read-only or disabled, an exception is thrown.
+ * Clicks the button, but only if it is actually possible to do so by the user.
+ * If the button is read-only or disabled, an exception is thrown.
+ *
+ * See [_checkClickable] for more details.
  * @throws IllegalStateException if the button was not visible or not enabled.
  */
 fun Button._click() {
-    checkEditableByUser()
+    _checkClickable()
     click()
+}
+
+/**
+ * Alias for [checkEditableByUser].
+ */
+fun Button._checkClickable() {
+    checkEditableByUser()
 }

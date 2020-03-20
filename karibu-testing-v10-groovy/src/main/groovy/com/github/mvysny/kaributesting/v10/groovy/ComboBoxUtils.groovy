4bed1a9526cce21c8f856b2754593bbd56721c31
Martin Vysny <mavi@vaadin.com>
2020-03-21 00:35:53 +0200

    groovy: support for ComboBox

---
+++ karibu-testing-v10-groovy/src/main/groovy/com/github/mvysny/kaributesting/v10/groovy/ComboBoxUtils.groovy
@@ -0,0 +1,58 @@
+package com.github.mvysny.kaributesting.v10.groovy
+
+import com.github.mvysny.kaributesting.v10.ComboBoxKt
+import com.vaadin.flow.component.combobox.ComboBox
+import com.vaadin.flow.component.select.Select
+import groovy.transform.CompileStatic
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
+
+/**
+ * @author mavi
+ */
+@CompileStatic
+class ComboBoxUtils {
+    /**
+     * Emulates an user inputting something into the combo box, filtering items.  You can use [getSuggestionItems]
+     * to retrieve those items and to verify that the filter on your data provider works properly.
+     */
+    static void setUserInput(@NotNull ComboBox self, @Nullable String userInput) {
+        ComboBoxKt.setUserInput(self, userInput)
+    }
+
+    /**
+     * Simulates the user creating a custom item. Only works if the field is editable by the user
+     * and allows custom values ([ComboBox.isAllowCustomValue] is true).
+     */
+    static void _fireCustomValueSet(@NotNull ComboBox self, @NotNull String userInput) {
+        ComboBoxKt._fireCustomValueSet(self, userInput)
+    }
+
+    /**
+     * Fetches items currently displayed in the suggestion box.
+     */
+    static <T> List<T> getSuggestionItems(@NotNull ComboBox<T> self) {
+        ComboBoxKt.getSuggestionItems(self)
+    }
+
+    /**
+     * Fetches captions of items currently displayed in the suggestion box.
+     */
+    static List<String> getSuggestions(@NotNull ComboBox self) {
+        ComboBoxKt.getSuggestions(self)
+    }
+
+    /**
+     * Fetches items currently displayed in the suggestion box.
+     */
+    static <T> List<T> getSuggestionItems(@NotNull Select<T> self) {
+        ComboBoxKt.getSuggestionItems(self)
+    }
+
+    /**
+     * Fetches captions of items currently displayed in the suggestion box.
+     */
+    static List<String> getSuggestions(@NotNull Select self) {
+        ComboBoxKt.getSuggestions(self)
+    }
+}
---
+++ karibu-testing-v10-groovy/src/test/groovy/com/github/mvysny/kaributesting/v10/groovy/ComboBoxUtilsTest.groovy
@@ -0,0 +1,40 @@
+package com.github.mvysny.kaributesting.v10.groovy
+
+import com.github.mvysny.kaributesting.v10.MockVaadin
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.combobox.ComboBox
+import com.vaadin.flow.component.select.Select
+import groovy.transform.CompileStatic
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+/**
+ * Merely a test of the API. No need to test the implementation, it is thoroughly tested elsewhere.
+ * @author mavi
+ */
+@CompileStatic
+class ComboBoxUtilsTest {
+    @BeforeEach void setup() { MockVaadin.setup() }
+    @AfterEach void teardown() { MockVaadin.tearDown() }
+
+    @Test
+    void apiTest() {
+        new ComboBox().setUserInput("foo")
+        def combobox = new ComboBox()
+        combobox.setAllowCustomValue(true)
+        combobox._fireCustomValueSet("foo")
+        def comboWithStrings = new ComboBox<String>()
+        comboWithStrings.setItems(["a", "b", "c"])
+        List<String> items = comboWithStrings.getSuggestionItems()
+        def comboWithObjects = new ComboBox<Object>()
+        comboWithObjects.setItems([new Object()])
+        items = comboWithObjects.getSuggestions()
+        def selectWithStrings = new Select<String>()
+        selectWithStrings.setItems([])
+        items = selectWithStrings.getSuggestionItems()
+        def selectWithObjects = new Select<Object>()
+        selectWithObjects.setItems([])
+        items = selectWithObjects.getSuggestions()
+    }
+}

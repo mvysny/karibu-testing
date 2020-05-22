4b6f7381d58a715210155c8488a6350d9a84f11d
Martin Vysny <mavi@vaadin.com>
2020-05-22 13:31:35 +0300

    Add navigator tests for postponed navigation

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/NavigatorTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/NavigatorTest.kt
@@ -3,6 +3,9 @@ package com.github.mvysny.kaributesting.v10
 import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.dynatest.expectThrows
 import com.github.mvysny.karibudsl.v10.navigateToView
+import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.router.BeforeLeaveEvent
 import kotlin.test.expect
 
 internal fun DynaNodeGroup.navigatorTest() {
@@ -23,6 +26,34 @@ internal fun DynaNodeGroup.navigatorTest() {
         expectView<ParametrizedView>()
         expectThrows(AssertionError::class) {
             expectView<TestingView>()
+        }
+    }
+
+    // tests for https://github.com/mvysny/karibu-testing/issues/34
+    group("delayed navigation") {
+        test("view") {
+            var a: BeforeLeaveEvent.ContinueNavigationAction? = null
+            UI.getCurrent().addBeforeLeaveListener { e ->
+                expect(null) { a }
+                a = e.postpone()
+            }
+            navigateToView<TestingView>()
+            _expectNone<TestingView>()
+            a!!.proceed()
+            _expectOne<TestingView>()
+        }
+
+        test("notification") {
+            var a: BeforeLeaveEvent.ContinueNavigationAction? = null
+            UI.getCurrent().addBeforeLeaveListener { e ->
+                expect(null) { a }
+                a = e.postpone()
+                Notification.show("postponing")
+            }
+            navigateToView<TestingView>()
+            expectNotifications("postponing")
+            a!!.proceed()
+            expectNoNotifications()
         }
     }
 }

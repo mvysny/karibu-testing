5278a310c9f0a9dca31ac284db9145d6f5b435ec
Martin Vysny <mavi@vaadin.com>
2020-06-11 22:01:55 +0300

    bye bye Vaadin 13

--- karibu-testing-v10/kt10-testrun-vaadin13/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-    testImplementation(platform("com.vaadin:vaadin-bom:${properties["vaadin13_version"]}"))
-    testImplementation("com.vaadin:vaadin:${properties["vaadin13_version"]}")
-    testImplementation(project(":karibu-testing-v10:kt10-tests"))
-}

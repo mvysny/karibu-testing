a03189f12e0ea945f64ce0a14665a522f511eb43
Martin Vysny <mavi@vaadin.com>
2020-07-01 11:53:15 +0300

    1.1.27

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.1.27-SNAPSHOT"
+    version = "1.1.27"
     repositories {
         jcenter()
         maven { setUrl("https://maven.vaadin.com/vaadin-prereleases/") }

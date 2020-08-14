61a5434b4d8d81e507d06eca7759e7046fd33afc
Martin Vysny <mavi@vaadin.com>
2020-08-14 17:18:26 +0300

    1.1.30-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.1.29"
+    version = "1.1.30-SNAPSHOT"
     repositories {
         jcenter()
         maven { setUrl("https://maven.vaadin.com/vaadin-prereleases/") }

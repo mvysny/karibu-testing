9dba53726d6a7c0800799ac636e0e70d3ff70fc7
Martin Vysny <mavi@vaadin.com>
2020-09-09 10:30:53 +0300

    1.2.6-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.2.5"
+    version = "1.2.6-SNAPSHOT"
     repositories {
         jcenter()
         mavenCentral()

a506e0fb321083bb81d8b87e64f318e4828e7a19
Martin Vysny <mavi@vaadin.com>
2020-10-13 14:40:40 +0300

    MockLitTemplateParser: initial port to ByteBuddy

--- karibu-testing-v10/kt10-testrun-vaadin18/src/test/kotlin/MockLitTemplateParser.kt
+++ karibu-testing-v10/kt10-testrun-vaadin18/src/test/kotlin/MockLitTemplateParser.kt
@@ -2,8 +2,12 @@ package com.vaadin.flow.component.littemplate;
 
 import com.github.mvysny.kaributesting.v10.mock.MockInstantiator
 import com.github.mvysny.kaributesting.v10.mock.MockNpmTemplateParser
-import com.vaadin.flow.component.polymertemplate.TemplateParser
 import com.vaadin.flow.di.Instantiator
+import net.bytebuddy.ByteBuddy
+import net.bytebuddy.implementation.MethodCall
+import net.bytebuddy.implementation.bytecode.assign.Assigner
+import net.bytebuddy.matcher.ElementMatchers
+import java.lang.reflect.Method
 
 private class MockLitTemplateParser : LitTemplateParserImpl() {
     /**
@@ -13,24 +17,67 @@ private class MockLitTemplateParser : LitTemplateParserImpl() {
     override fun getSourcesFromTemplate(tag: String, url: String): String? {
         return MockNpmTemplateParser.mockGetSourcesFromTemplate(tag, url)
     }
-
-    companion object Factory : LitTemplateParser.LitTemplateParserFactory() {
-        override fun createParser(): LitTemplateParser = MockLitTemplateParser()
-    }
 }
 
-private object MockNpmTemplateParserFactory : TemplateParser.TemplateParserFactory() {
-    override fun createParser(): TemplateParser = MockNpmTemplateParser()
+object ByteBuddyUtils {
+    /**
+     * Subclasses [baseClass] and overrides [methodName] which will now return [withResult].
+     */
+    fun overrideMethod(baseClass: Class<*>, methodName: String, withResult: ()->Any?): Class<*> {
+        return ByteBuddy().subclass(baseClass)
+                .method(ElementMatchers.named(methodName))
+                .intercept(MethodCall.call(withResult))
+                .make()
+                .load(ByteBuddyUtils::class.java.classLoader)
+                .loaded
+    }
+
+    /**
+     * Subclasses [baseClass] and overrides [methodName] which will now return the outcome of [delegate].
+     */
+    fun overrideMethod(baseClass: Class<*>, methodName: String, delegate: Method): Class<*> {
+        return ByteBuddy().subclass(baseClass)
+                .method(ElementMatchers.named(methodName))
+                .intercept(MethodCall.invoke(delegate).withAssigner(Assigner.DEFAULT, Assigner.Typing.DYNAMIC))
+                .make()
+                .load(ByteBuddyUtils::class.java.classLoader)
+                .loaded
+    }
 }
 
 class MockInstantiatorV18(delegate: Instantiator): MockInstantiator(delegate) {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : Any?> getOrCreate(type: Class<T>): T = when (type) {
-        LitTemplateParser.LitTemplateParserFactory::class.java ->
-            MockLitTemplateParser.Factory as T
-        TemplateParser.TemplateParserFactory::class.java ->
-            MockNpmTemplateParserFactory as T
+        classLitTemplateParserFactory ->
+            classMockLitTemplateParserFactory.newInstance() as T
+        classNpmTemplateParserFactory ->
+            classMockNpmTemplateParserFactory.newInstance() as T
         else -> super.getOrCreate(type)
+    }
+
+    companion object {
+        /**
+         * The `LitTemplateParser.LitTemplateParserFactory` class.
+         */
+        private val classLitTemplateParserFactory: Class<*> =
+                Class.forName("com.vaadin.flow.component.littemplate.LitTemplateParser${'$'}LitTemplateParserFactory")
+        /**
+         * The `LitTemplateParser.LitTemplateParserFactory` class returning `MockLitTemplateParser`.
+         */
+        private val classMockLitTemplateParserFactory: Class<*> =
+                ByteBuddyUtils.overrideMethod(classLitTemplateParserFactory, "createParser") { MockLitTemplateParser() }
+
+        /**
+         * The `TemplateParser.TemplateParserFactory` class.
+         */
+        private val classNpmTemplateParserFactory: Class<*> =
+                Class.forName("com.vaadin.flow.component.polymertemplate.TemplateParser${'$'}TemplateParserFactory")
+
+        /**
+         * The `TemplateParser.TemplateParserFactory` class returning [MockNpmTemplateParser].
+         */
+        private val classMockNpmTemplateParserFactory: Class<*> =
+                ByteBuddyUtils.overrideMethod(classNpmTemplateParserFactory, "createParser") { MockNpmTemplateParser() }
     }
 }

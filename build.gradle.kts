f34c589c0f36bc3d50602a62e3df753bcb1fca80
Martin Vysny <mavi@vaadin.com>
2021-02-04 15:34:05 +0200

    publish directly to maven central since bintray is terminated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.*
 
 plugins {
     kotlin("jvm") version "1.4.30"
-    id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
-    id("org.jetbrains.dokka") version "1.4.0"
+    signing
 }
 
 defaultTasks("clean", "build")
@@ -16,7 +13,6 @@ allprojects {
     group = "com.github.mvysny.kaributesting"
     version = "1.2.9-SNAPSHOT"
     repositories {
-        jcenter()
         mavenCentral()
         maven(url = "https://maven.vaadin.com/vaadin-addons")
         maven(url = "https://maven.vaadin.com/vaadin-prereleases/")
@@ -27,31 +23,31 @@ subprojects {
     apply {
         plugin("maven-publish")
         plugin("kotlin")
-        plugin("com.jfrog.bintray")
-        plugin("org.jetbrains.dokka")
+        plugin("org.gradle.signing")
     }
 
     // creates a reusable function which configures proper deployment to Bintray
     ext["configureBintray"] = { artifactId: String ->
 
-        val local = Properties()
-        val localProperties: File = rootProject.file("local.properties")
-        if (localProperties.exists()) {
-            localProperties.inputStream().use { local.load(it) }
+        java {
+            withJavadocJar()
+            withSourcesJar()
         }
 
-        val sourceJar = task("sourceJar", Jar::class) {
-            dependsOn(tasks["classes"])
-            archiveClassifier.set("sources")
-            from(sourceSets.main.get().allSource)
-        }
-
-        val javadocJar = task("javadocJar", Jar::class) {
-            from(tasks["dokkaJavadoc"])
-            archiveClassifier.set("javadoc")
+        tasks.withType<Javadoc> {
+            isFailOnError = false
         }
 
         publishing {
+            repositories {
+                maven {
+                    setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                        password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+                    }
+                }
+            }
             publications {
                 create("mavenJava", MavenPublication::class.java).apply {
                     groupId = project.group.toString()
@@ -81,27 +77,12 @@ subprojects {
                     }
 
                     from(components["java"])
-                    artifact(sourceJar)
-                    artifact(javadocJar)
                 }
             }
         }
 
-        bintray {
-            user = local.getProperty("bintray.user")
-            key = local.getProperty("bintray.key")
-            pkg(closureOf<BintrayExtension.PackageConfig> {
-                repo = "github"
-                name = "com.github.mvysny.kaributesting"
-                setLicenses("Apache-2.0")
-                vcsUrl = "https://github.com/mvysny/karibu-testing"
-                publish = true
-                setPublications("mavenJava")
-                version(closureOf<BintrayExtension.VersionConfig> {
-                    this.name = project.version.toString()
-                    released = Date().toString()
-                })
-            })
+        signing {
+            sign(publishing.publications["mavenJava"])
         }
     }
 

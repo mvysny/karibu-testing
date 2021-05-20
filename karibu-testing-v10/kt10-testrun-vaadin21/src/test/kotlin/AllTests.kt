42eb486fcd97017f04f67dff2725fd41f90238ad
Martin Vysny <mavi@vaadin.com>
2021-05-20 09:17:23 +0300

    Add Vaadin 21 tests

---
+++ karibu-testing-v10/kt10-testrun-vaadin21/src/test/kotlin/AllTests.kt
@@ -0,0 +1,23 @@
+import com.github.mvysny.dynatest.DynaTest
+import com.github.mvysny.kaributesting.v10.*
+import com.github.mvysny.kaributesting.v10.mock.MockVaadinHelper
+import java.net.URL
+import kotlin.test.expect
+
+class AllTests : DynaTest({
+    test("flow-build-info.json exists") {
+        val res: URL? = Thread.currentThread().contextClassLoader.getResource("META-INF/VAADIN/config/flow-build-info.json")
+        expect(true, "flow-build-info.json is not on the classpath!") { res != null }
+    }
+
+    group("Vaadin env") {
+        beforeEach { MockVaadin.setup() }
+        afterEach { MockVaadin.tearDown() }
+
+        test("Vaadin version") {
+            expect(21) { VaadinMeta.version }
+            expect(false) { VaadinMeta.isCompatibilityMode }
+        }
+    }
+    allTests19(isModuleTest = false)
+})

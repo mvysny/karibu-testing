07c78c099f8dccdadc0c7bc5655456ded9fd15f0
Martin Vysny <mavi@vaadin.com>
2021-06-22 11:53:50 +0200

    minor

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/MenuBarTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/MenuBarTest.kt
@@ -12,25 +12,22 @@ internal fun DynaNodeGroup.menuBarTestbatch() {
     beforeEach { MockVaadin.setup() }
     afterEach { MockVaadin.tearDown() }
 
-    // MenuBar is present in Vaadin 14+ only
-    if (VaadinMeta.version >= 14) {
-        test("_clickItemWithCaption") {
-            var clicked = 0
-            val menuBar: MenuBar = UI.getCurrent().menuBar {
-                item("foo", { _ -> clicked++ }) {}
-            }
-            menuBar._clickItemWithCaption("foo")
-            expect(1) { clicked }
+    test("_clickItemWithCaption") {
+        var clicked = 0
+        val menuBar: MenuBar = UI.getCurrent().menuBar {
+            item("foo", { _ -> clicked++ }) {}
         }
+        menuBar._clickItemWithCaption("foo")
+        expect(1) { clicked }
+    }
 
-        test("_click") {
-            var clicked = 0
-            lateinit var mi: MenuItem
-            val menuBar: MenuBar = UI.getCurrent().menuBar {
-                mi = item("foo", { _ -> clicked++ }) {}
-            }
-            menuBar._click(mi)
-            expect(1) { clicked }
+    test("_click") {
+        var clicked = 0
+        lateinit var mi: MenuItem
+        val menuBar: MenuBar = UI.getCurrent().menuBar {
+            mi = item("foo", { _ -> clicked++ }) {}
         }
+        menuBar._click(mi)
+        expect(1) { clicked }
     }
 }

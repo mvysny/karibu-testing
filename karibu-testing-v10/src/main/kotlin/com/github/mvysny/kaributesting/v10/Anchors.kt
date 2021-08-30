e3eb57691bb5b7793951e78d7c6f1a4c4a9ea047
Martin Vysny <mavi@vaadin.com>
2021-08-30 10:41:43 +0300

    vaadin v bump

--- karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/Anchors.kt
+++ karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/Anchors.kt
@@ -2,6 +2,7 @@ package com.github.mvysny.kaributesting.v10
 
 import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.html.Anchor
+import java.lang.reflect.Field
 
 /**
  * Navigates to where this router link points to. The difference to [_click] is that this one doesn't check whether
@@ -20,16 +21,21 @@ public fun Anchor.click() {
     UI.getCurrent().navigate(href)
 }
 
+/**
+ * First "fix" for https://github.com/vaadin/flow/issues/10924 (the introduction
+ * of "disabledHref") is now being reversed for another solution. So, if the field
+ * exists, use it, otherwise just return the contents of [Anchor.getHref].
+ */
+private val _Anchor_disabledHref: Field? =
+    Anchor::class.java.declaredFields.firstOrNull { it.name == "disabledHref" } ?.apply {
+        isAccessible = true
+    }
+
 public var Anchor._href: String
     get() {
         var hr: String = href
-        if (VaadinMeta.fullVersion >= SemanticVersion(20, 0, 4) ||
-            (VaadinMeta.version == 14 && VaadinMeta.fullVersion.isAtLeast(14, 7))) {
-            // work around https://github.com/vaadin/flow/issues/10924
-            val disabledHrefField =
-                Anchor::class.java.getDeclaredField("disabledHref")
-            disabledHrefField.isAccessible = true
-            val disabledHref = disabledHrefField.get(this) as String?
+        if (_Anchor_disabledHref != null) {
+            val disabledHref = _Anchor_disabledHref.get(this) as String?
             if (disabledHref != null) {
                 hr = disabledHref
             }

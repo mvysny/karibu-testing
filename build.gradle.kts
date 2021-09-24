5ef8e748f1cb1ff529b513c94f44e7c6279ee65e
Martin Vysny <mavi@vaadin.com>
2021-09-24 19:56:03 +0300

    1.3.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.3.4"
+    version = "1.3.5-SNAPSHOT"
     repositories {
         mavenCentral()
         maven(url = "https://maven.vaadin.com/vaadin-addons")

b356ae178c34718d6447c48c616b14697c78e0c2
Martin Vysny <mavi@vaadin.com>
2021-12-04 14:35:10 +0200

    remove tests for vaadin 20

--- karibu-testing-v10/kt10-testrun-vaadin20-module/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    testImplementation("com.vaadin:vaadin:${properties["vaadin20_version"]}")
-    testImplementation(project(":karibu-testing-v10:kt19-tests")) {
-        exclude(group = "com.github.appreciated")
-    }
-}
--- karibu-testing-v10/kt10-testrun-vaadin20/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    testImplementation("com.vaadin:vaadin:${properties["vaadin20_version"]}")
-    testImplementation(project(":karibu-testing-v10:kt19-tests"))
-
-    // for testing out the NPM template loading from META-INF/resources/frontend/
-    testImplementation("com.github.appreciated:app-layout-addon:4.0.0.beta5")
-}

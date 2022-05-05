060d661a8cdbc18e77d69e4aa15a818ed5caac89
Martin Vysny <mavi@vaadin.com>
2022-05-05 11:44:34 +0300

    skip vaadin 23 tests on java 8

--- karibu-testing-v10/kt10-testrun-vaadin23.next/build.gradle.kts
+++ karibu-testing-v10/kt10-testrun-vaadin23.next/build.gradle.kts
@@ -5,3 +5,6 @@ dependencies {
     // for testing out the NPM template loading from META-INF/resources/frontend/
     testImplementation("com.github.appreciated:app-layout-addon:4.0.0.beta5")
 }
+
+// Vaadin 23 requires Java 11 or higher
+tasks.test { onlyIf { JavaVersion.current() >= JavaVersion.VERSION_11 } }

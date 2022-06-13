61d1a4c47914f439900417fa0b8e83d4bbda7108
Martin Vysny <mavi@vaadin.com>
2022-06-13 08:54:35 +0300

    kt19-tests now compiles against vaadin 23

--- karibu-testing-v10/kt19-tests/build.gradle.kts
+++ karibu-testing-v10/kt19-tests/build.gradle.kts
@@ -3,5 +3,5 @@ dependencies {
         exclude(group = "com.vaadin")
     }
     api("com.vaadin:vaadin-spring:12.2.0")
-    api("com.vaadin:vaadin:${properties["vaadin22_version"]}")
+    compileOnly("com.vaadin:vaadin:${properties["vaadin23_next_version"]}")
 }
--- karibu-testing-v10/kt19-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/Renderers22Test.kt
+++ karibu-testing-v10/kt19-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/Renderers22Test.kt
@@ -2,13 +2,7 @@ package com.github.mvysny.kaributesting.v10
 
 import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.dynatest.DynaTestDsl
-import com.vaadin.flow.component.UI
-import com.vaadin.flow.component.html.Span
-import com.vaadin.flow.data.renderer.ComponentRenderer
 import com.vaadin.flow.data.renderer.LitRenderer
-import com.vaadin.flow.data.renderer.TemplateRenderer
-import com.vaadin.flow.data.renderer.TextRenderer
-import com.vaadin.flow.function.SerializableFunction
 import kotlin.test.expect
 
 @DynaTestDsl

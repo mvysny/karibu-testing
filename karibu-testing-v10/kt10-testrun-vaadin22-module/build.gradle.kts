80faa19974e98d20874d68631f9ab3ba4eacacbb
Martin Vysny <mavi@vaadin.com>
2022-06-18 14:17:59 +0300

    #116: move kt19-tests to karibu-testing-v23

--- karibu-testing-v10/kt10-testrun-vaadin22-module/build.gradle.kts
+++ karibu-testing-v10/kt10-testrun-vaadin22-module/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     testImplementation("com.vaadin:vaadin:${properties["vaadin22_version"]}")
-    testImplementation(project(":karibu-testing-v10:kt19-tests")) {
+    testImplementation(project(":karibu-testing-v23:kt23-tests")) {
         exclude(group = "com.github.appreciated")
     }
 }
--- karibu-testing-v23/kt23-tests/build.gradle.kts
+++ karibu-testing-v23/kt23-tests/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     api(project(":karibu-testing-v10:kt10-tests")) {
         exclude(group = "com.vaadin")
     }
+    api(project(":karibu-testing-v23"))
     api("com.vaadin:vaadin-spring:12.2.0")
     compileOnly("com.vaadin:vaadin:${properties["vaadin23_next_version"]}")
 }

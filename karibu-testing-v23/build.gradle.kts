f61c281d2620084331797cfa48573a09d63702fe
Martin Vysny <mavi@vaadin.com>
2022-06-18 13:25:24 +0300

    create karibu-testing-v23 project; move vaadin23 tests there

---
+++ karibu-testing-v23/build.gradle.kts
@@ -0,0 +1,21 @@
+dependencies {
+    // - don't compile-depend on vaadin-core anymore: the app itself should manage Vaadin dependencies, for example
+    //   using the gradle-flow-plugin or direct dependency on vaadin-core. The reason is that the app may wish to use the
+    //   npm mode and exclude all webjars.
+    // - depend on "vaadin" instead of just "vaadin-core", to bring in Grid Pro.
+    // - depend on the latest Vaadin (Vaadin 23 LTS)
+    compileOnly("com.vaadin:vaadin:${properties["vaadin23_version"]}")
+    testImplementation("com.vaadin:vaadin:${properties["vaadin23_version"]}")
+    api(project(":karibu-testing-v10"))
+
+    testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
+    testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+}
+
+kotlin {
+    explicitApi()
+}
+
+val configureBintray = ext["configureBintray"] as (artifactId: String) -> Unit
+configureBintray("karibu-testing-v23")
+

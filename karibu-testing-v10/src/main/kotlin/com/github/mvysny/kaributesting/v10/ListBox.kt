18e4e6bb7349bed909da7f45b45ad21e93d8c5f0
Martin Vysny <mavi@vaadin.com>
2022-09-27 09:57:27 +0300

    Add ListBox.getItems()

--- karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/ListBox.kt
+++ karibu-testing-v10/src/main/kotlin/com/github/mvysny/kaributesting/v10/ListBox.kt
@@ -6,6 +6,11 @@ import com.vaadin.flow.component.listbox.ListBoxBase
  * Fetches renderings of items currently displayed in the list box component.
  */
 public fun <T> ListBoxBase<*, T, *>.getRenderedItems(): List<String?> {
-    val items: List<T> = dataProvider._findAll()
+    val items: List<T> = getItems()
     return items.map { itemRenderer._getPresentationValue(it) }
 }
+
+/**
+ * Fetches all items currently displayed in the list box component.
+ */
+public fun <T> ListBoxBase<*, T, *>.getItems(): List<T> = dataProvider._findAll()

c0948288a7824bacf2dca3ef7dd891c6e578a910
Martin Vysny <mavi@vaadin.com>
2022-10-04 15:10:21 +0300

    1.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.3.21-SNAPSHOT"
+    version = "1.3.21"
     repositories {
         mavenCentral()
         maven(url = "https://maven.vaadin.com/vaadin-addons")

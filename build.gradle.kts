1f7c6fa37cff76daf41f506717560f2319dbe90a
Martin Vysny <mavi@vaadin.com>
2022-12-01 08:53:45 +0200

    #136 configure javac+kotlinc to target java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ subprojects {
     }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "17"
     }
 
     tasks.withType<Test> {
@@ -101,8 +101,8 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     configurations.implementation {

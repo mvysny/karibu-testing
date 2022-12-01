c37b80a4bf36f61654f61e974b986e5bc189e2f0
Martin Vysny <mavi@vaadin.com>
2022-12-01 14:56:59 +0200

    #136 remove the old javax.servlet mock

--- mock-servlet-environment/src/test/kotlin/com/github/mvysny/kaributesting/mockhttp/Utils.kt
+++
@@ -1,9 +0,0 @@
-package com.github.mvysny.kaributesting.mockhttp
-
-import java.security.Principal
-
-data class MockPrincipal(private val name: String, val allowedRoles: List<String> = listOf()) : Principal {
-    override fun getName(): String = name
-
-    fun isUserInRole(role: String): Boolean = allowedRoles.contains(role)
-}

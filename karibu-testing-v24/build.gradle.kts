4bebe5edfc08a399ab56cc3f82df640822f9aa52
Martin Vysny <mavi@vaadin.com>
2022-12-02 14:00:32 +0200

    karibu-testing-v24: add dependency on karibu-testing-v23

--- karibu-testing-v24/build.gradle.kts
+++ karibu-testing-v24/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compileOnly("com.vaadin:vaadin:${properties["vaadin24_version"]}")
     testImplementation("com.vaadin:vaadin:${properties["vaadin24_version"]}")
-    // api(project(":karibu-testing-v23"))
+    api(project(":karibu-testing-v23"))
 
     testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
     testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")

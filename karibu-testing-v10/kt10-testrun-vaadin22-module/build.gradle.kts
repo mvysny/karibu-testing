7e885f6cc317d7585f645822792f3c1877c64dd4
Martin Vysny <mavi@vaadin.com>
2023-06-07 11:00:17 +0300

    Remove support for Vaadin 22 - the maintenance of Vaadin 22 ended at March 2023

--- karibu-testing-v10/kt10-testrun-vaadin22-module/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    testImplementation("com.vaadin:vaadin:${properties["vaadin22_version"]}")
-    testImplementation(project(":karibu-testing-v23:kt23-tests")) {
-        exclude(group = "com.github.appreciated")
-    }
-}

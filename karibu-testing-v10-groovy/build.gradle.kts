0c3ca34a88716ee3d7c176f18ad4bf8108b70bcc
Martin Vysny <mavi@vaadin.com>
2023-06-07 11:12:28 +0300

    Remove support for Vaadin 22 - the maintenance of Vaadin 22 ended at March 2023

--- karibu-testing-v10-groovy/build.gradle.kts
+++ karibu-testing-v10-groovy/build.gradle.kts
@@ -10,9 +10,9 @@ dependencies {
     // 1. don't compile-depend on vaadin-core anymore: the app itself should manage Vaadin dependencies, for example
     // using the gradle-flow-plugin or direct dependency on vaadin-core. The reason is that the app may wish to use the
     // npm mode and exclude all webjars.
-    // 2. Keep Vaadin 22 dependency: Vaadin 23 requires Java11+
-    compileOnly("com.vaadin:vaadin-core:${properties["vaadin22_version"]}")
-    testImplementation("com.vaadin:vaadin-core:${properties["vaadin22_version"]}")
+    // 2. Don't depend on Vaadin 23: it requires Java11+ and Groovy fails for some reason.
+    compileOnly("com.vaadin:vaadin-core:${properties["vaadin14_version"]}")
+    testImplementation("com.vaadin:vaadin-core:${properties["vaadin14_version"]}")
 
     api(project(":karibu-testing-v10"))
 
--- karibu-testing-v10-pro-groovy/build.gradle.kts
+++ karibu-testing-v10-pro-groovy/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 dependencies {
     api(project(":karibu-testing-v10-groovy"))
 
-    // don't compile-depend on vaadin-core anymore: the app itself should manage Vaadin dependencies, for example
+    // 1. don't compile-depend on vaadin-core anymore: the app itself should manage Vaadin dependencies, for example
     // using the gradle-flow-plugin or direct dependency on vaadin-core. The reason is that the app may wish to use the
     // npm mode and exclude all webjars.
     // depend on vaadin instead of vaadin-core, to bring in Confirm Dialog and Grid Pro.
-    compileOnly("com.vaadin:vaadin:${properties["vaadin22_version"]}")
-    testImplementation("com.vaadin:vaadin:${properties["vaadin22_version"]}")
+    // 2. Don't depend on Vaadin 23: it requires Java11+ and Groovy fails for some reason.
+    compileOnly("com.vaadin:vaadin:${properties["vaadin14_version"]}")
+    testImplementation("com.vaadin:vaadin:${properties["vaadin14_version"]}")
 
     testImplementation("org.junit.jupiter:junit-jupiter:${properties["junit5_version"]}")
     testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")

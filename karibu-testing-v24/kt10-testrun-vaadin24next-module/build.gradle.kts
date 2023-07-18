93f37dd7aaf97d8f2813a13d20bdd516453fc625
Martin Vysny <mavi@vaadin.com>
2023-07-18 08:45:17 +0200

    vaadin v bump

--- karibu-testing-v24/kt10-testrun-vaadin24next-module/build.gradle.kts
+++ karibu-testing-v24/kt10-testrun-vaadin24next-module/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     testImplementation("com.vaadin:vaadin:${properties["vaadin24next_version"]}")
-    testImplementation("com.vaadin:vaadin-spring:${properties["vaadin24next_version"]}")
+    testImplementation("com.vaadin:vaadin-spring:${properties["vaadin24next_version_spring"]}")
     testImplementation(project(":karibu-testing-v23:kt23-tests")) {
         exclude(group = "com.github.appreciated")
     }
--- karibu-testing-v24/kt10-testrun-vaadin24next/build.gradle.kts
+++ karibu-testing-v24/kt10-testrun-vaadin24next/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     testImplementation("com.vaadin:vaadin:${properties["vaadin24next_version"]}")
-    testImplementation("com.vaadin:vaadin-spring:${properties["vaadin24next_version"]}")
+    testImplementation("com.vaadin:vaadin-spring:${properties["vaadin24next_version_spring"]}")
     testImplementation(project(":karibu-testing-v23:kt23-tests"))
 }

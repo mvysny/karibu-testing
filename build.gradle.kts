55b6edadd34671c02c967b4a52505beec6bb42ed
Martin Vysny <mavi@vaadin.com>
2024-01-19 16:13:02 +0200

    2.1.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ if (JavaVersion.current() < JavaVersion.VERSION_17) {
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "2.1.2"
+    version = "2.1.3-SNAPSHOT"
     repositories {
         mavenCentral()
         maven(url = "https://maven.vaadin.com/vaadin-addons")

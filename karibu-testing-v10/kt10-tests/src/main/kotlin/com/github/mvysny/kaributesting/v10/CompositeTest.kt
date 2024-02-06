0de4ad578f0bb6dcb4dc9234d62ee5c23d98b2c1
Martin Vysny <mavi@vaadin.com>
2024-02-06 08:29:20 +0200

    #162 more tests

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/CompositeTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/CompositeTest.kt
@@ -4,21 +4,52 @@ import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.dynatest.DynaTestDsl
 import com.vaadin.flow.component.Composite
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.dialog.Dialog
+import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.orderedlayout.FlexLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
 
 @DynaTestDsl
 internal fun DynaNodeGroup.compositeTests() {
     beforeEach { MockVaadin.setup() }
     afterEach { MockVaadin.tearDown() }
 
-    test("Composite<Dialog> causes footer components to appear two times in Karibu tree dump") {
-        class MyDialog : Composite<Dialog>() {
+    test("Composite<Button> doesn't duplicate the button") {
+        class MyButton : Composite<Button>()
+
+        val btn = MyButton()
+        btn._expectOne<Button>()
+    }
+
+    test("Composite<VerticalLayout> doesn't duplicate its children") {
+        class MyLayout : Composite<VerticalLayout>() {
             init {
-                content.footer.add(Button("Hi!"))
+                content.add(Button("Foo"))
             }
         }
 
-        val dlg = MyDialog()
-        dlg._expectOne<Button> { text = "Hi!" }
+        val l = MyLayout()
+        l._expectOne<Button>()
+    }
+
+    test("Composite<FlexLayout> doesn't duplicate its children") {
+        class MyLayout : Composite<FlexLayout>() {
+            init {
+                content.add(Button("Foo"))
+            }
+        }
+
+        val l = MyLayout()
+        l._expectOne<Button>()
+    }
+
+    test("Composite<Div> doesn't duplicate its children") {
+        class MyLayout : Composite<Div>() {
+            init {
+                content.add(Button("Foo"))
+            }
+        }
+
+        val l = MyLayout()
+        l._expectOne<Button>()
     }
 }

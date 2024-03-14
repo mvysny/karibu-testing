338f009715a21297f3d9d7e69f3ec85c5de2eda3
Martin Vysny <mavi@vaadin.com>
2024-03-14 15:35:11 +0200

    extract deps to libs.versions.toml

--- karibu-testing-v10/build.gradle.kts
+++ karibu-testing-v10/build.gradle.kts
@@ -4,18 +4,18 @@ dependencies {
     //   npm mode and exclude all webjars.
     // - depend on "vaadin" instead of just "vaadin-core", to bring in Grid Pro.
     // - depend on the lowest Vaadin (Vaadin 14 LTS)
-    compileOnly("com.vaadin:vaadin:${properties["vaadin14_version"]}")
-    testImplementation("com.vaadin:vaadin:${properties["vaadin14_version"]}")
+    compileOnly(libs.vaadin.v14)
+    testImplementation(libs.vaadin.v14)
 
     api(libs.fake.servlet)
-    api("com.github.mvysny.karibu-tools:karibu-tools:${properties["karibu_tools_version"]}")
+    api(libs.karibu.tools)
 
-    testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
+    testImplementation(libs.dynatest)
     api(kotlin("test"))
-    testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    testImplementation(libs.slf4j.simple)
 
     // to have the class autodiscovery functionality
-    implementation("io.github.classgraph:classgraph:4.8.149")
+    implementation(libs.classgraph)
 }
 
 kotlin {

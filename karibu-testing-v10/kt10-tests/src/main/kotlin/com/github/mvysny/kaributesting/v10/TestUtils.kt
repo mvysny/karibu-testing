635dea2d14e4d162d3efe73c18facfb549d7136b
Martin Vysny <mavi@vaadin.com>
2024-03-15 08:27:23 +0200

    fix compiler err

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/TestUtils.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/TestUtils.kt
@@ -6,6 +6,6 @@ import java.io.PrintStream
 /**
  * Prints everything to an in-memory buffer. Call [toString] to obtain what was printed.
  */
-class InMemoryPrintStream(private val bout: ByteArrayOutputStream = ByteArrayOutputStream()) : PrintStream(bout, true, Charsets.UTF_8) {
+class InMemoryPrintStream(private val bout: ByteArrayOutputStream = ByteArrayOutputStream()) : PrintStream(bout, true, "UTF-8") {
     override fun toString(): String = String(bout.toByteArray(), Charsets.UTF_8)
 }

fa348a728d1bd5f7d40a6391b731d39b470fc120
Martin Vysny <mavi@vaadin.com>
2024-08-07 16:07:05 +0300

    ContextMenu: improve compatibility

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/ContextMenuTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/ContextMenuTest.kt
@@ -388,6 +388,28 @@ internal fun DynaNodeGroup.contextMenuTestbatch() {
             expect(2) { called }
         }
 
+        test("setOpened(true) succeeds on null item") {
+            var called = 0
+            lateinit var cm: GridContextMenu<String>
+            UI.getCurrent().grid<String> {
+                cm = gridContextMenu {
+                    item("click me")
+                    setDynamicContentHandler { item ->
+                        expect(null) { item }
+                        expect(0) { called }
+                        called = 1
+                        true
+                    }
+                }
+            }
+            cm.addGridContextMenuOpenedListener { e ->
+                expect(1) { called }
+                called = 2
+            }
+            cm.setOpened(true, null)
+            expect(2) { called }
+        }
+
         test("setOpened(false) fires GridContextMenuOpenedEvent") {
             var called = false
             lateinit var cm: GridContextMenu<String>

916624faccf18ca4c499c5b75a4168c17d370ffc
Martin Vysny <mavi@vaadin.com>
2024-08-07 09:26:27 +0300

    1.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.kaributesting"
-    version = "1.4.4-SNAPSHOT"
+    version = "1.4.4"
     repositories {
         mavenCentral()
         maven(url = "https://maven.vaadin.com/vaadin-addons")

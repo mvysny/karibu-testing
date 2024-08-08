cadf5cd1b6ceeed3fccef522b49aee351f7927d1
Martin Vysny <mavi@vaadin.com>
2024-08-08 09:28:21 +0300

    fix javadoc

--- karibu-testing-v10-spring/src/main/java/com/github/mvysny/kaributesting/v10/spring/MockSpringServletService.java
+++ karibu-testing-v10-spring/src/main/java/com/github/mvysny/kaributesting/v10/spring/MockSpringServletService.java
@@ -17,15 +17,25 @@ import org.springframework.context.ApplicationContext;
  * A mocking service that performs three very important tasks:
  * <ul>
  *     <li>Overrides {@link #isAtmosphereAvailable} to tell Vaadin that we don't have Atmosphere (otherwise Vaadin will crash)</li>
- * <li>Provides some dummy value as a root ID via {@link #getMainDivId} (otherwise the mocked servlet env will crash).</li>
- * <li>Provides a {@link MockSpringVaadinSession} instead of {@link com.vaadin.flow.spring.SpringVaadinSession}.</li>
+ *     <li>Provides some dummy value as a root ID via {@link #getMainDivId} (otherwise the mocked servlet env will crash).</li>
+ *     <li>Provides a {@link MockSpringVaadinSession} instead of {@link com.vaadin.flow.spring.SpringVaadinSession}.</li>
  * </ul>
  * The class is intentionally opened, to be extensible in user's library.
  */
 public class MockSpringServletService extends SpringVaadinServletService {
+    /**
+     * Creates new UIs.
+     */
     @NotNull
     private final Function0<UI> uiFactory;
 
+    /**
+     * Creates new testing service.
+     * @param servlet the testing servlet
+     * @param deploymentConfiguration the configuration to use
+     * @param ctx the Spring application context
+     * @param uiFactory     Creates new UIs.
+     */
     public MockSpringServletService(@NotNull MockSpringServlet servlet,
                                     @NotNull DeploymentConfiguration deploymentConfiguration,
                                     @NotNull ApplicationContext ctx,

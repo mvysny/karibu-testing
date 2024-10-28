0b690333d7cb8c900d6b8b916dd8a9d5555f1fc7
Martin Vysny <mavi@vaadin.com>
2024-10-28 14:46:15 +0200

    replace dynatest with junit5

--- karibu-testing-v23/kt23-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/AllTests19.kt
+++ karibu-testing-v23/kt23-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/AllTests19.kt
@@ -1,16 +1,10 @@
 package com.github.mvysny.kaributesting.v10
 
-import com.github.mvysny.dynatest.DynaNodeGroup
-import com.github.mvysny.dynatest.DynaTestDsl
 import org.junit.jupiter.api.Nested
 
 /**
  * @param isModuleTest if true then this test run simulates a jar reusable component.
  */
-@DynaTestDsl
-fun DynaNodeGroup.allTests19(isModuleTest: Boolean) {
-}
-
 abstract class AbstractAllTests19(val isModuleTest: Boolean) {
     @Nested inner class AllTests10 : AbstractAllTests10(isModuleTest)
     @Nested inner class GridVaadin19Tests : AbstractGrid19Tests()
--- karibu-testing-v24/kt10-testrun-vaadin24next-module/src/test/kotlin/AllTests.kt
+++ karibu-testing-v24/kt10-testrun-vaadin24next-module/src/test/kotlin/AllTests.kt
@@ -1,5 +1,3 @@
-import com.github.mvysny.dynatest.DynaTest
-import com.github.mvysny.dynatest.jvmVersion
 import com.github.mvysny.kaributesting.v10.*
 import com.github.mvysny.kaributools.VaadinVersion
 import org.junit.jupiter.api.AfterEach
@@ -9,25 +7,7 @@ import org.junit.jupiter.api.Test
 import java.net.URL
 import kotlin.test.expect
 
-class AllTests : DynaTest({
-    test("flow-build-info.json doesn't exist") {
-        val res: URL? = Thread.currentThread().contextClassLoader.getResource("META-INF/VAADIN/config/flow-build-info.json")
-        expect(null, "flow-build-info.json exists on the classpath!") { res }
-    }
-
-    group("Vaadin env") {
-        beforeEach { MockVaadin.setup() }
-        afterEach { MockVaadin.tearDown() }
-
-        test("Vaadin version") {
-            expect(24) { VaadinVersion.get.major }
-            expect(false) { VaadinMeta.isCompatibilityMode }
-        }
-    }
-    allTests19(isModuleTest = true)
-})
-
-class AllTests24 {
+class AllTests {
     @Test fun `flow-build-info-json doesn't exist`() {
         val res: URL? = Thread.currentThread().contextClassLoader.getResource("META-INF/VAADIN/config/flow-build-info.json")
         expect(null, "flow-build-info.json exists on the classpath!") { res }

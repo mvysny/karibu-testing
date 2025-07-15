aeb4b8749708e2febf17cf8533ac829db8717da2
Martin Vysny <mavi@vaadin.com>
2025-07-15 10:10:40 +0300

    Add tests for MessageList

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/MessagesTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/MessagesTest.kt
@@ -1,6 +1,9 @@
 package com.github.mvysny.kaributesting.v10
 
+import com.github.mvysny.karibudsl.v10.messageList
+import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.messages.MessageInput
+import com.vaadin.flow.component.messages.MessageListItem
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
@@ -10,6 +13,18 @@ import kotlin.test.expect
 abstract class AbstractMessageTests() {
     @BeforeEach fun fakeVaadin() { MockVaadin.setup() }
     @AfterEach fun tearDownVaadin() { MockVaadin.tearDown() }
+
+    @Nested inner class `message list` {
+        @Test fun smoke() {
+            UI.getCurrent().messageList {}
+        }
+
+        @Test fun smoke2() {
+            val ml = UI.getCurrent().messageList {}
+            ml.isMarkdown = true
+            ml.addItem(MessageListItem("# Hello!"))
+        }
+    }
 
     @Nested inner class MessageInputTests {
         @Test fun `submit event`() {

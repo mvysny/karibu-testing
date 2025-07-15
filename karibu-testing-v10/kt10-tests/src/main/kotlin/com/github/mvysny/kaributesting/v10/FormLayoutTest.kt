1cf50cd67b1196b00d555a570de09f409dd81b96
Martin Vysny <mavi@vaadin.com>
2025-07-15 09:07:18 +0300

    FormLayout: add tests for form row

--- karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/FormLayoutTest.kt
+++ karibu-testing-v10/kt10-tests/src/main/kotlin/com/github/mvysny/kaributesting/v10/FormLayoutTest.kt
@@ -34,18 +34,69 @@ abstract class AbstractFormLayoutTests() {
         expect(tf) { f._get<FormLayout.FormItem> { label = "foo" } .field }
     }
 
-    @Nested inner class `FormItem-field` {
-        @Test fun `succeeds with one field and no label`() {
+    @Nested inner class formitem {
+        @Test
+        fun `lookup-of-component-from-within-formitem`() {
             val tf = TextField()
-            val f: FormLayout.FormItem = FormLayout().addFormItem(tf, "foo")
-            expect(tf) { f._get<FormLayout.FormItem> { label = "foo" } .field }
+            val fl = FormLayout()
+            val f: FormLayout.FormItem = fl.addFormItem(tf, "foo")
+            expect(tf) { f._get<TextField>() }
+            expect(tf) { fl._get<TextField>() }
+        }
+
+        /**
+         * Tests [field].
+         */
+        @Nested inner class `FormItem-field` {
+            @Test
+            fun `succeeds with one field and no label`() {
+                val tf = TextField()
+                val fl = FormLayout()
+                val f: FormLayout.FormItem = fl.addFormItem(tf, "foo")
+                expect(tf) {
+                    f._get<FormLayout.FormItem> {
+                        label = "foo"
+                    }.field
+                }
+                expect(tf) {
+                    fl._get<FormLayout.FormItem> {
+                        label = "foo"
+                    }.field
+                }
+            }
+
+            @Test
+            fun `fails with no field`() {
+                val f: FormLayout.FormItem =
+                    FormLayout().addFormItem(Select<String>(), "foo")
+                f.removeAll()
+                expectThrows(
+                    IllegalStateException::class,
+                    "FormItem: Expected 1 field but got 0. Component tree:\n└── FormItem[]"
+                ) {
+                    f.field
+                }
+            }
+        }
+    }
+
+    /**
+     * New stuff in Vaadin 24.8t
+     */
+    @Nested inner class formrow {
+        @Test fun `lookup`() {
+            val tf = TextField()
+            val fl = FormLayout()
+            val f: FormLayout.FormRow = fl.addFormRow(tf)
+            expect(tf) { f._get<TextField>() }
+            expect(tf) { fl._get<TextField>() }
         }
         @Test fun `fails with no field`() {
-            val f: FormLayout.FormItem = FormLayout().addFormItem(Select<String>(), "foo")
+            val fl = FormLayout()
+            val f: FormLayout.FormRow = fl.addFormRow(TextField())
             f.removeAll()
-            expectThrows(IllegalStateException::class, "FormItem: Expected 1 field but got 0. Component tree:\n└── FormItem[]") {
-                f.field
-            }
+            f._expectNone<TextField>()
+            fl._expectNone<TextField>()
         }
     }
 }
